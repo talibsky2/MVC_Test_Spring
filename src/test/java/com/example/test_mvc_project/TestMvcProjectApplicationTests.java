30ce7b6d30406adedb45174468f83ee197f5704c
Vladimir <tollenkon@ukr.net>
2022-11-14 13:01:18 +0200

    First

---
+++ src/test/java/com/example/test_mvc_project/TestMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_mvc_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMvcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
